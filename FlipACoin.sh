b8607032ff239af222a3379ed4552ebf8a22a1ec
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-05 03:22:35 +0530

    ADDED: Singlet calculate store in dictionary

--- FlipACoin.sh
+++ FlipACoin.sh
@@ -1,10 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash
+
+#Constant
+HEAD=1
+TAIL=0
+
+declare -A singlet
+
+#Function
+function readValue() {
+	read -p "Enter how many times flip the coin : " noOfTimeFlipCoin
+}
 
 #Function to display head or tail
 function displayHeadTail() {
-	randomCheck=$((RANDOM%2))
+	randomFlip=$((RANDOM%2))
 
-	if [[ $randomCheck -eq 1 ]]
+	if [[ $randomFlip -eq $HEAD ]]
 	then
 		echo "Head"
 	else
@@ -12,4 +23,32 @@ function displayHeadTail() {
 	fi
 }
 
+function singlet() {
+	readValue
+	countHead=0
+	countTail=0
+	for (( flip=1; flip<=noOfTimeFlipCoin; flip++ ))
+	do
+		randomFlip=$((RANDOM%2))
+		if [ $randomFlip -eq $HEAD ]
+		then
+			((countHead++))
+		else
+			((countTail++))
+		fi
+	done
+	#Calculate head and tail percent
+	headPercent="$countHead *100 / $noOfTimeFlipCoin"
+	tailPercent="$countTail *100 / $noOfTimeFlipCoin"
+	#Store head and tail percentage in dictionary
+	singlet[headPer]=$headPercent
+	singlet[tailPer]=$tailPercent
+	#Display head and tail percentage
+	echo "headPer : " ${singlet[headPer]}
+	echo "tailPer : " ${singlet[tailPer]}
+
+	echo $singlet
+}
+
 displayHeadTail
+singlet
